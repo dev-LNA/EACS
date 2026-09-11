e74900b3e66c3960302ffaf4bad4ab944e3a8a85
Denis <denis.bernardes099@gmail.com>
2026-09-11 16:20:41 -0300

    fixing tests

--- src/python/tests/integration_tests/test_everything.py
+++ src/python/tests/integration_tests/test_everything.py
@@ -4,12 +4,15 @@ import unittest
 import numpy as np
 from astropy.io import fits
 from header_formatter.header import (
+    EICS,
     GUI,
     S4ICS,
     TCS,
     Focuser,
+    General_ECHARPE_KWs,
     General_SPARC4_KWs,
     Weather_Station,
+    iKon_L,
     iXon_Ultra,
 )
 from header_formatter.post_processor import Post_Processor
@@ -26,7 +29,7 @@ from header_formatter.utils import (
 
 
 class Test_Everything(unittest.TestCase):
-    def setUp(self) -> None:
+    def test_fill_sparc4_header(self) -> None:
 
         self._hdr_data = dict.fromkeys(
             ["CCD", "GUI", "ICS", "FOCUSER", "WSTATION", "GENERAL KW", "TCS"], "{}"
@@ -67,11 +70,59 @@ class Test_Everything(unittest.TestCase):
             obj.check_allowed_values()
             self.hdr = obj.fill_image_header(self.hdr)
 
-        hdu = fits.PrimaryHDU(np.zeros((1, 1), dtype=np.uint16), self.hdr)
+        hdu = fits.PrimaryHDU(np.zeros((1, 1), dtype=np.uint16), self.hdr)  # type: ignore
         hdu = Post_Processor._fix_standard_keywords(hdu)
         self.hdr = hdu.header
 
-    def test_fill_image_header(self) -> None:
+        if "" in self.hdr.values():
+            print(repr(self.hdr))
+            assert False
+
+    def test_fill_echarpe_header(self) -> None:
+
+        self._hdr_data = dict.fromkeys(
+            ["CCD", "GUI", "ICS", "FOCUSER", "WSTATION", "GENERAL KW", "TCS"], "{}"
+        )
+        self._hdr_data["CCD"] = json.dumps(ccd_kw)
+        self._hdr_data["GUI"] = json.dumps(s4gui_json)
+        self._hdr_data["WSTATION"] = json.dumps(WS_json)
+        self._hdr_data["FOCUSER"] = json.dumps(focuser_json)
+        self._hdr_data["ICS"] = json.dumps(ics_kw)
+        self._hdr_data["TCS"] = json.dumps(tcs_json)
+        self._hdr_data["GENERAL KW"] = json.dumps(general_kw)
+
+        setup = Header_Class_Setup("echarpe")
+        self.hdr, hdr_data, hdr_cnt, log_file, file_name = setup.create_setup(
+            json.dumps(self._hdr_data), "00000000_s4c1_000001.fits"
+        )
+
+        for obj in [
+            Focuser,
+            EICS,
+            GUI,
+            TCS,
+            Weather_Station,
+            General_ECHARPE_KWs,
+            iKon_L,
+        ]:
+            kws_specs = setup.create_hdr_specs(obj.name)
+            obj = obj(kws_specs, hdr_cnt, log_file, file_name)
+            obj.write_header_all_apps(hdr_data)
+            obj.get_app_header_data()
+            obj.fix_original_string()
+            obj.load_json()
+            obj.fix_original_hdr_data()
+            obj.extract_data()
+            obj.fix_keywords()
+            obj.fix_remainder_keywords()
+            obj.check_kws_types()
+            obj.check_allowed_values()
+            self.hdr = obj.fill_image_header(self.hdr)
+
+        hdu = fits.PrimaryHDU(np.zeros((1, 1), dtype=np.uint16), self.hdr)  # type: ignore
+        hdu = Post_Processor._fix_standard_keywords(hdu)
+        self.hdr = hdu.header
+
         if "" in self.hdr.values():
             print(repr(self.hdr))
             assert False
